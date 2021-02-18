31df380ad5d67ef1299badf938e6708a41ab7f95
herouu <herouucn@foxmail.com>
2021-02-18 16:36:07 +0800

    验证码生成 替换为 easy-captcha

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CaptchaController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CaptchaController.java
@@ -1,21 +1,18 @@
 package com.ruoyi.web.controller.common;
 
-import com.google.code.kaptcha.Producer;
+
 import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.redis.RedisCache;
-import com.ruoyi.common.utils.sign.Base64;
 import com.ruoyi.common.utils.uuid.IdUtils;
+import com.wf.captcha.ArithmeticCaptcha;
+import com.wf.captcha.SpecCaptcha;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.util.FastByteArrayOutputStream;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
@@ -26,11 +23,6 @@ import java.util.concurrent.TimeUnit;
  */
 @RestController
 public class CaptchaController {
-    @Resource(name = "captchaProducer")
-    private Producer captchaProducer;
-
-    @Resource(name = "captchaProducerMath")
-    private Producer captchaProducerMath;
 
     @Autowired
     private RedisCache redisCache;
@@ -48,32 +40,27 @@ public class CaptchaController {
         String uuid = IdUtils.simpleUUID();
         String verifyKey = Constants.CAPTCHA_CODE_KEY + uuid;
 
-        String capStr = null, code = null;
-        BufferedImage image = null;
+        String code = null;
+        String image = null;
 
         // 生成验证码
         if ("math".equals(captchaType)) {
-            String capText = captchaProducerMath.createText();
-            capStr = capText.substring(0, capText.lastIndexOf("@"));
-            code = capText.substring(capText.lastIndexOf("@") + 1);
-            image = captchaProducerMath.createImage(capStr);
+            ArithmeticCaptcha captcha = new ArithmeticCaptcha(130, 48);
+            // captcha.setLen(3);  // 几位数运算，默认是两位
+            // captcha.getArithmeticString();  // 获取运算的公式：3+2=?
+            code = captcha.text();
+            image = captcha.toBase64();
         } else if ("char".equals(captchaType)) {
-            capStr = code = captchaProducer.createText();
-            image = captchaProducer.createImage(capStr);
+            SpecCaptcha specCaptcha = new SpecCaptcha(130, 48, 5);
+            code = specCaptcha.text().toLowerCase();
+            image = specCaptcha.toBase64();
         }
 
         redisCache.setCacheObject(verifyKey, code, Constants.CAPTCHA_EXPIRATION, TimeUnit.MINUTES);
-        // 转换流信息写出
-        FastByteArrayOutputStream os = new FastByteArrayOutputStream();
-        try {
-            ImageIO.write(image, "jpg", os);
-        } catch (IOException e) {
-            return AjaxResult.error(e.getMessage());
-        }
 
         AjaxResult ajax = AjaxResult.success();
         ajax.put("uuid", uuid);
-        ajax.put("img", Base64.encode(os.toByteArray()));
+        ajax.put("img", image);
         return ajax;
     }
 }
